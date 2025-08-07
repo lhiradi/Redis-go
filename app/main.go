e1382cf38eb0755b4ff41007634f3d2c5f0eaed1
lhiradi <hiradjalilian@gmail.com>
2025-08-07 16:39:39 +0330

    .

--- app/main.go
+++ app/main.go
@@ -15,14 +15,28 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
+	defer l.Close()
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("+PONG\r\n"))
+
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from connection")
+			return
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
