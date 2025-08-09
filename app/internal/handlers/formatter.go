d7a75406b75a30e25c83930bde579d69cda94557
lhiradi <hiradjalilian@gmail.com>
2025-08-09 17:06:21 +0330

    implemented XREAD

--- app/internal/handlers/formatter.go
+++ app/internal/handlers/formatter.go
@@ -27,20 +27,9 @@ func formatStreamEntries(entries []db.StreamEntry) string {
 
 func formatXReadEntries(streamKey string, entries []db.StreamEntry) string {
 	var builder strings.Builder
-	builder.WriteString(fmt.Sprintf("*%d\r\n", 1))
-	builder.WriteString(fmt.Sprintf("*%d\r\n", len(entries)+1)) // entries + streamKey
+	builder.WriteString(fmt.Sprintf("*%d\r\n", 2)) // Array of two elements: stream key and entries
 	builder.WriteString(fmt.Sprintf("$%d\r\n%s\r\n", len(streamKey), streamKey))
-	builder.WriteString(fmt.Sprintf("*%d\r\n", len(entries)))
-	for _, entry := range entries {
-		// Array for each entry: [ID, [field1, value1, field2, value2]]
-		builder.WriteString(fmt.Sprintf("*2\r\n$%d\r\n%s\r\n", len(entry.ID), entry.ID))
-		builder.WriteString(fmt.Sprintf("*%d\r\n", len(entry.Fields)*2))
-
-		for key, value := range entry.Fields {
-			builder.WriteString(fmt.Sprintf("$%d\r\n%s\r\n", len(key), key))
-			builder.WriteString(fmt.Sprintf("$%d\r\n%s\r\n", len(value), value))
-		}
-	}
+	builder.WriteString(formatStreamEntries(entries))
 
 	return builder.String()
 }
