24a9a577ec637a801b538cfa3a8268bb2847d495
lhiradi <hiradjalilian@gmail.com>
2025-08-09 16:28:53 +0330

    implemented XRANGE

--- app/internal/handlers/formatter.go
+++ app/internal/handlers/formatter.go
@@ -27,9 +27,9 @@ func formatStreamEntries(entries []db.StreamEntry) string {
 
 func formatXReadEntries(streamKey string, entries []db.StreamEntry) string {
 	var builder strings.Builder
-	builder.WriteString(fmt.Sprintf("*%d\r\n", 1))
-	builder.WriteString(fmt.Sprintf("*%d\r\n", len(entries)+1)) // entries + streamKey
+	builder.WriteString(fmt.Sprintf("*%d\r\n", 1)) // entries + streamKey
 	builder.WriteString(fmt.Sprintf("$%d\r\n%s\r\n", len(streamKey), streamKey))
+	builder.WriteString(fmt.Sprintf("*%d\r\n", len(entries)))
 	for _, entry := range entries {
 		// Array for each entry: [ID, [field1, value1, field2, value2]]
 		builder.WriteString(fmt.Sprintf("*2\r\n$%d\r\n%s\r\n", len(entry.ID), entry.ID))
