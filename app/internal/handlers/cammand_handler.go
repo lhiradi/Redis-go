ebeddf1f6452af0ed0ab98e460cabd46ca3c9278
lhiradi <hiradjalilian@gmail.com>
2025-08-12 13:59:33 +0330

    implemented SUBSCRIBE

--- app/internal/handlers/cammand_handler.go
+++ app/internal/handlers/cammand_handler.go
@@ -453,6 +453,6 @@ func handleSubscribe(args []string, DB *db.DB, activeTx *transaction.Transaction
 	if len(args) < 2 {
 		return "", nil, fmt.Errorf(" wrong number of arguments for 'SUBSCRIBE' command")
 	}
-	response := fmt.Sprintf("*3\r\n$9\r\nsubscribe\r\n$3\r\n%s\r\n:1\r\n", args[1])
+	response := fmt.Sprintf("*3\r\n$9\r\nsubscribe\r\n$%d\r\n%s\r\n:1\r\n", len(args[1]), args[1])
 	return response, nil, nil
 }
